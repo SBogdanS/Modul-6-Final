515779251d2c2368cd9eaaf0f32f4f61bed378f4
SBogdanS <sbodia09@gmail.com>
2023-10-31 15:00:31 +0000

    fin

---
+++ file_parser.py
@@ -0,0 +1,108 @@
+import sys
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+AVI_VIDEO= []
+MP4_VIDEO= []
+MOV_VIDEO= []
+MKV_VIDEO= []
+DOC_DOCUMENTS=[]
+DOCX_DOCUMENTS=[]
+TXT_DOCUMENTS=[]
+PDF_DOCUMENTS=[]
+XLSX_DOCUMENTS=[]
+PPTX_DOCUMENTS=[]
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
+MY_OTHER = []
+ARCHIVES = []
+
+
+REGISTER_EXTENSION = {
+    'JPEG': JPEG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'AVI': AVI_VIDEO,
+    'MP4': MP4_VIDEO,
+    'MOV': MOV_VIDEO,
+    'MKV': MKV_VIDEO,
+    'DOC': DOC_DOCUMENTS,
+    'DOCX': DOCX_DOCUMENTS,
+    'PDF': PDF_DOCUMENTS,
+    'TXT': TXT_DOCUMENTS,
+    'XLSX': XLSX_DOCUMENTS,
+    'PPTX': PPTX_DOCUMENTS,
+    'OGG': OGG_AUDIO,
+    'MP3': MP3_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
+    'ZIP': ARCHIVES,
+    'GZ': ARCHIVES,
+    'TAR': ARCHIVES,
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+
+def get_extension(name: str) -> str:
+    return Path(name).suffix[1:].upper()  # suffix[1:] -> .jpg -> jpg
+
+def scan(folder: Path):
+    for item in folder.iterdir():
+        # Робота з папкою
+        if item.is_dir():  # перевіряємо чи обєкт папка
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+
+        # Робота з файлом
+        extension = get_extension(item.name)  # беремо розширення файлу
+        full_name = folder / item.name  # беремо повний шлях до файлу
+        if not extension:
+            MY_OTHER.append(full_name)
+        else:
+            try:
+                REGISTER_EXTENSION[extension]
+                EXTENSIONS.add(extension)
+            except KeyError:
+                UNKNOWN.add(extension) 
+                MY_OTHER.append(full_name)
+
+if __name__ == '__main__':
+    folder_process = sys.argv[1]
+    scan(Path(folder_process))
+    print(f'Images jpeg: {JPEG_IMAGES}')
+    print(f'Images jpg: {JPG_IMAGES}')
+    print(f'Images png: {PNG_IMAGES}')
+    print(f'Images svg: {SVG_IMAGES}')
+    print(f'Video avi: {AVI_VIDEO}')
+    print(f'Video mp4: {MP4_VIDEO}')
+    print(f'Video mov: {MOV_VIDEO}')
+    print(f'Video mkv: {MKV_VIDEO}')
+    print(f'Documents doc: {DOC_DOCUMENTS}')
+    print(f'Documents docx: {DOCX_DOCUMENTS}')
+    print(f'Documents txt: {TXT_DOCUMENTS}')
+    print(f'Documents pdf: {PDF_DOCUMENTS}')
+    print(f'Documents xlsx: {XLSX_DOCUMENTS}')
+    print(f'Documents pptx: {PPTX_DOCUMENTS}')    
+    print(f'Audio mp3: {MP3_AUDIO}')
+    print(f'Audio ogg: {OGG_AUDIO}')
+    print(f'Audio wav: {WAV_AUDIO}')
+    print(f'Audio amr: {AMR_AUDIO}')       
+    print(f'Archives zip: {ARCHIVES}')
+    print(f'Archives gz: {ARCHIVES}')
+    print(f'Archives tar: {ARCHIVES}')
+    print(f'EXTENSIONS: {EXTENSIONS}')
+    print(f'UNKNOWN: {UNKNOWN}')
+
+
+
